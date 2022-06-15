9ecf1ff51d17e1cba4cdf564b530fa01553d2b6c
Eslam85861 <105951306+Eslam85861@users.noreply.github.com>
2022-06-15 13:21:54 +0200

    Revert "Add notes details"

--- app/src/main/java/com/example/mental_health_app/yoga/model/YogaModel.kt
+++ app/src/main/java/com/example/mental_health_app/yoga/model/YogaModel.kt
@@ -1,7 +1,6 @@
 package com.example.mental_health_app.yoga.model
 
-import java.util.*
+import android.graphics.drawable.Drawable
+import android.media.Image
 
-
-data class YogaModel(val id: Int, val yogaName: String , val yogaDescription: String ,
-                     val yogaImage: Int , val steps: String)
+data class YogaModel(val yogaName: String , val yogaDescription: String , val yogaImage: Int)
--- app/src/main/java/com/example/mental_health_app/yoga/view/adapter/YogaAdapter.kt
+++ app/src/main/java/com/example/mental_health_app/yoga/view/adapter/YogaAdapter.kt
@@ -6,15 +6,9 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mental_health_app.databinding.YogaRowBinding
 import com.example.mental_health_app.yoga.model.YogaModel
-import com.example.notesapp.notes.model.NoteModel
 
-class YogaAdapter(private val onItemClicked: (Int) -> Unit ): RecyclerView.Adapter<YogaViewHolder>()  {
+class YogaAdapter(val yogaList: ArrayList<YogaModel>): RecyclerView.Adapter<YogaViewHolder>()  {
 
-    private var yogaList = mutableListOf<YogaModel>()
-    fun setYoga(yoga: List<YogaModel>) {
-        yogaList = yoga.toMutableList()
-        notifyDataSetChanged()
-    }
     lateinit var context: Context
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): YogaViewHolder {
         context = parent.context
@@ -28,9 +22,6 @@ class YogaAdapter(private val onItemClicked: (Int) -> Unit ): RecyclerView.Adapt
         holder.binding.tvYogaName.text=item.yogaName
         holder.binding.tvYogaDescription.text=item.yogaDescription
         holder.binding.ivYoga.setImageResource(item.yogaImage)
-        holder.itemView.setOnClickListener{
-            onItemClicked(item.id)
-        }
     }
 
     override fun getItemCount(): Int {
